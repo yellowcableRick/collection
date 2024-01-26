67bf5dce4b09c09ab60dcba0e8c63f678b389183
Rick Pennings <rick@yellowcable.nl>
2024-01-26 07:21:24 +0100

    Implemented update counter

--- src/Traits/Calculation/Counter/CounterTrait.php
+++ src/Traits/Calculation/Counter/CounterTrait.php
@@ -52,12 +52,15 @@ trait CounterTrait
     }
 
     /**
-     * Execute all registered counters.
+     * Execute all registered counters. Will zero out all counters before running.
      *
-     * @return $this
+     * @return static
      */
     public function executeCount(): static
     {
+        foreach ($this->getCounters() as $counter) {
+            $counter->result = 0;
+        }
         foreach ($this as $item) {
             foreach ($this->getCounters() as $counter) {
                 !$counter->closure->getClosure()($item) ?: $counter->result++;
@@ -67,11 +70,20 @@ trait CounterTrait
     }
 
     /**
-     * @throws NotImplementedException
+     * Updates all registered counters. Will NOT zero out all counters before running.
+     * May be interesting to use after aggregation and collecting new items, to append
+     * the counters with the new values.
+     *
+     * @returns static
      */
     public function updateCount(): static
     {
-        throw new NotImplementedException();
+        foreach ($this as $item) {
+            foreach ($this->getCounters() as $counter) {
+                !$counter->closure->getClosure()($item) ?: $counter->result++;
+            }
+        }
+        return $this;
     }
 
     /**
